9ec1eafa7bcfecd53e231a29a982e9bd952718b2
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-04-30 12:40:06 +0300

    Initial Commit

---
+++ PatternFlyWeight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PatternFlyWeight
+//
+//  Created by Артем on 4/30/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
