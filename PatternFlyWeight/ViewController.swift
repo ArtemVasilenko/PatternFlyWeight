5c6d980fcb289241dbd58811367301541623f957
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-04-30 12:51:32 +0300

    +

--- PatternFlyWeight/ViewController.swift
+++ PatternFlyWeight/ViewController.swift
@@ -1,10 +1,4 @@
-//
-//  ViewController.swift
-//  PatternFlyWeight
-//
-//  Created by Артем on 4/30/19.
-//  Copyright © 2019 Артем. All rights reserved.
-//
+
 
 import UIKit
 
@@ -12,9 +6,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let color1 = UIColor.red
+        let color2 = UIColor.red
+        
+        let color3 = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+        let color4 = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+        
+        let color5 = UIColor.Rgba(red: 1, green: 0, blue: 0, alpha: 1)
+        let color6 = UIColor.Rgba(red: 1, green: 0, blue: 0, alpha: 1)
+        
+        print(color1===color2)
+        print(color3===color4)
+        print(color5===color6) //сравнение объектов по памяти
     }
-
-
+    
 }
 
+
+extension UIColor {
+    static var colorStore: [String:UIColor] = [:]
+    class func Rgba(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) -> UIColor {
+    
+        let key = "\(red) \(green) \(blue) \(alpha)"
+        
+        if let color = colorStore[key] {
+            return color
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: alpha)
+        
+        colorStore[key] = color
+        return color
+    }
+    
+}
